24b91bac331ec81c2881d86bdd38ebd1e147db7f
Adam Zhao <adam.liang@alumni.stonybrook.edu>
2023-02-16 17:55:42 -0500

    final commit

--- alert.js
+++ alert.js
@@ -1,36 +1,39 @@
+function convertText(string) {
+  let tmpArr = string.split("\n");
+  tmpArr = tmpArr.map((el) => {
+    let temp = "";
+    const l = el.length;
+    const ref = "banana";
+    for (let i = 0, b = 0; i < l; i++, b++) {
+      if (el[i] === " ") {
+        temp += " ";
+        b = 0;
+      }
+      temp += ref[b % ref.length];
+    }
+    return temp;
+  });
 
-function convertText(string){
-    let tmpArr = string.split('\n');
-    tmpArr = tmpArr.map(el => {
-        let temp = '';
-        const l = el.length
-        const ref = "poo";
-        for(let i = 0, b = 0; i < l; i++, b++){
-            if(el[i] === ' ') {
-                temp += ' '
-                b = 0
-            }
-            temp += ref[b%ref.length];
-        }
-        return temp        
-    })
-    
-    return tmpArr.join('\n');
+  return tmpArr.join("\n");
 }
 
-const nodes = document.querySelectorAll("a")
-nodes.forEach(
-    a => a.innerText = convertText(a.innerText)
-)
+document.querySelector("body").style.backgroundColor = "black";
 
-const nodes2 = document.querySelectorAll('p')
-nodes2.forEach(
-    a => a.innerText = convertText(a.innerText)
-)
+const nodes = document.querySelectorAll("a");
+nodes.forEach((a) => (a.innerText = convertText(a.innerText)));
+nodes.forEach((a) => (a.style = "color:yellow"));
 
-const imageNodes = document.querySelectorAll('img')
+const nodes2 = document.querySelectorAll("p");
+nodes2.forEach((a) => (a.innerText = convertText(a.innerText)));
+nodes2.forEach((a) => (a.style = "color:yellow"));
 
-imageNodes.forEach(el => {
-    // el.setAttribute('src', 'https://media.cnn.com/api/v1/images/stellar/prod/120604032828-fresh-ripe-bananas.jpg?q=x_0,y_106,h_2019,w_3590,c_crop/h_720,w_1280');
-    //el.style.backgroundImage("url('https://media.cnn.com/api/v1/images/stellar/prod/120604032828-fresh-ripe-bananas.jpg?q=x_0,y_106,h_2019,w_3590,c_crop/h_720,w_1280')");
-})
+const nodes3 = document.querySelectorAll("h1");
+nodes3.forEach((a) => (a.innerText = convertText(a.innerText)));
+nodes3.forEach((a) => (a.style = "color:yellow"));
+
+const imageNodes = document.querySelectorAll("img");
+
+imageNodes.forEach((el) => {
+  // el.setAttribute('src', 'https://media.cnn.com/api/v1/images/stellar/prod/120604032828-fresh-ripe-bananas.jpg?q=x_0,y_106,h_2019,w_3590,c_crop/h_720,w_1280');
+  //el.style.backgroundImage("url('https://media.cnn.com/api/v1/images/stellar/prod/120604032828-fresh-ripe-bananas.jpg?q=x_0,y_106,h_2019,w_3590,c_crop/h_720,w_1280')");
+});
---
+++ talking_points.md
@@ -0,0 +1,4 @@
+1. our challenge was in finding how to access the dom in the current page through the chrome extension.
+2. figuring how to traverse through the nodes in order to find the elements that were text only
+   1. we ended up using innertext
+3. replacing images was harder as the srcset was overwriting the dom manipulation we were doing
